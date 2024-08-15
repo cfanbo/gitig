ec90d301b189610d62e5b7151549edb28f4675a9
cfanbo <haohtml@gmail.com>
2024-08-15 19:55:41 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,18 @@
 set -e +o pipefail
 
 
+show_help() {
+cat << EOF
+
+      gitig Gitignore CLI
+
+    -h          Display this help and exit
+
+    -V VERSION  Custom Software version. Default is 'latest'
+
+EOF
+}
+
 abort() {
   printf "%s\n" "$@" >&2
   exit 1
@@ -18,6 +30,28 @@ then
   CLI_ON_MACOS=1
 else
   abort "Currently is only supported on macOS and Linux."
+fi
+
+VERSION="latest"
+
+if [ $# != 0 ];
+then
+  while getopts "hV:-" o
+  do
+    case "$o" in
+      "h")
+        show_help
+        exit 0;
+        ;;
+      "V")
+        VERSION=v"$OPTARG"
+        ;;
+      *)
+        echo -e "Unexpected flag not supported"
+        exit 1
+        ;;
+    esac
+  done
 fi
 
 VERSION="latest"
@@ -42,11 +76,11 @@ then
   UNAME_MACHINE="$(/usr/bin/uname -m)"
   if [[ "${UNAME_MACHINE}" == "arm64" || "${UNAME_MACHINE}" == "aarch64" ]]
   then
-    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-aarch64-apple-darwin.tar.gz
-    tar zxf gitig-aarch64-apple-darwin.tar.gz
+    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-"$VERSION"-aarch64-apple-darwin.tar.gz
+    tar zxf gitig-"$VERSION"-aarch64-apple-darwin.tar.gz
   else
-    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-x86_64-apple-darwin.tar.gz
-    tar zxf gitig-x86_64-apple-darwin.tar.gz
+    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-"$VERSION"-x86_64-apple-darwin.tar.gz
+    tar zxf gitig-"$VERSION"-x86_64-apple-darwin.tar.gz
   fi
   mv ./gitig /usr/local/bin/
 fi
@@ -56,11 +90,11 @@ then
   UNAME_MACHINE="$(/usr/bin/uname -m)"
   if [[ "${UNAME_MACHINE}" == "arm64" || "${UNAME_MACHINE}" == "aarch64" ]]
   then
-    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-aarch64-unknown-linux-gnu.tar.gz
-    tar zxf gitig-aarch64-unknown-linux-gnu.tar.gz
+    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-"$VERSION"-aarch64-unknown-linux-gnu.tar.gz
+    tar zxf gitig-"$VERSION"-aarch64-unknown-linux-gnu.tar.gz
   else
-    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-x86_64-unknown-linux-gnu.tar.gz
-    tar zxf gitig-x86_64-unknown-linux-gnu.tar.gz
+    curl -O -fsSL https://githubfiles.oss-cn-shanghai.aliyuncs.com/gitig/gitig-"$VERSION"-x86_64-unknown-linux-gnu.tar.gz
+    tar zxf gitig-"$VERSION"-x86_64-unknown-linux-gnu.tar.gz
   fi
   mv ./gitig /usr/local/bin/
 fi
